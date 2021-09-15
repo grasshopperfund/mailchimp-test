55252c15e8c324dad4052f16718cfafecf537693
Nathaniel Louis Tisuela <natetisuela@gmail.com>
2021-09-15 14:39:54 -0700

    add vingcinq mailchimp marketing api

--- api.py
+++ api.py
@@ -2,9 +2,13 @@ import os
 
 from dotenv import load_dotenv
 import mailchimp_transactional as MailchimpTransactional
-
+from mailchimp3 import MailChimp
 
 load_dotenv()
 
 MAILCHIMP_TRANSACTIONAL_API_KEY = os.getenv("MAILCHIMP_TRANSACTIONAL_API_KEY")
 mailchimp_transactional_api = MailchimpTransactional.Client(MAILCHIMP_TRANSACTIONAL_API_KEY)
+
+MAILCHIMP_MARKETING_API_KEY = os.getenv("MAILCHIMP_MARKETING_API_KEY")
+MAILCHIMP_USERNAME = os.getenv("MAILCHIMP_USERNAME")
+mailchimp_marketing_vingt_cinq_api = MailChimp(mc_api=MAILCHIMP_MARKETING_API_KEY, mc_user=MAILCHIMP_USERNAME)
