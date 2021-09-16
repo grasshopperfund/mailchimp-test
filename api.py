afd3c1c636ba320aa335f09452e0c8844c003a14
Nathaniel Louis Tisuela <natetisuela@gmail.com>
2021-09-16 14:59:10 -0700

    add mailchimp marketing api client

--- api.py
+++ api.py
@@ -3,6 +3,7 @@ import os
 from dotenv import load_dotenv
 import mailchimp_transactional as MailchimpTransactional
 from mailchimp3 import MailChimp
+import mailchimp_marketing as MailchimpMarketing
 
 load_dotenv()
 
@@ -12,3 +13,10 @@ mailchimp_transactional_api = MailchimpTransactional.Client(MAILCHIMP_TRANSACTIO
 MAILCHIMP_MARKETING_API_KEY = os.getenv("MAILCHIMP_MARKETING_API_KEY")
 MAILCHIMP_USERNAME = os.getenv("MAILCHIMP_USERNAME")
 mailchimp_marketing_vingt_cinq_api = MailChimp(mc_api=MAILCHIMP_MARKETING_API_KEY, mc_user=MAILCHIMP_USERNAME)
+
+MAILCHIMP_SERVER_PREFIX = os.getenv("MAILCHIMP_SERVER_PREFIX")
+mailchimp_marketing_api = MailchimpMarketing.Client()
+mailchimp_marketing_api.set_config({
+    "api_key": MAILCHIMP_MARKETING_API_KEY,
+    "server": MAILCHIMP_SERVER_PREFIX
+})
